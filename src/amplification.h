656868d86c95d6b71f7d5a0ed9493528a5c04450
Juan José <jjparrad@gmail.com>
2025-12-03 17:00:28 +0100

    Add noise to amplification

--- src/amplification.h
+++ src/amplification.h
@@ -3,12 +3,22 @@
 #include "Vec3.h"
 #include "crust.h"
 #include "planet.h"
+#include "algorithm"
+
+#include "FastNoiseLite.h"
 
 class Amplification {
    public:
-    const float elevation_force = 0.2;
+    const float elevation_force = 0.1;
+    FastNoiseLite noise;
 
-    Amplification(Planet & p) { };
+    Amplification(Planet & p) {
+        noise.SetNoiseType(FastNoiseLite::NoiseType_OpenSimplex2);
+        noise.SetFrequency(4.0f);
+        noise.SetFractalType(FastNoiseLite::FractalType_FBm);
+        noise.SetFractalOctaves(3);
+        noise.SetSeed(2);
+    };
 
     void amplifyTerrain(Planet& planet) {
         Planet newPlanet(1.0f, planet.vertices.size() * 4);
@@ -40,7 +50,15 @@ class Amplification {
         float normalized_elevation = (crust_elevation - planet.min_elevation) / (planet.max_elevation - planet.min_elevation);
 
         newPlanet.amplified_elevations.push_back(crust_elevation);
-        
-        return vertexPosition * (1 + elevation_force * normalized_elevation);
+        Vec3 elevated_position = vertexPosition * (1 + elevation_force * normalized_elevation);
+        return addNoise(elevated_position);
     };
+
+    Vec3 addNoise(Vec3 position) {
+        float noiseRaw = noise.GetNoise(position[0], position[1], position[2]);
+        float n0 = (noiseRaw + 1.0f) * 0.5f;
+        float n = 1.0f + n0 * (elevation_force / 2);
+
+        return position * n;
+    }
 };
